676f8641e9715a05f1b07766fe45a6c5786f7f7b
aberiggs <sprocket710@gmail.com>
2023-08-08 23:26:14 -0700

    Initial Swift project

---
+++ Planning_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planning_BuddyApp.swift
+//  Planning Buddy
+//
+//  Created by Sprocket Riggs on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Planning_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
